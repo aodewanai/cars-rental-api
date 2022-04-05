3860abdba48096fcad5dc1e96b71d4c374b578cd
Tsapyk Yelyzaveta <lizatsapik@gmail.com>
2022-04-05 17:32:32 +0300

    + edit, delete car

--- routes/carRouter.js
+++ routes/carRouter.js
@@ -4,6 +4,8 @@ const CarController = require("../controllers/carController.js");
 
 
 router.post("/add", CarController.addCar);
+router.post("/edit", CarController.editCar);
+router.delete("/:id", CarController.deleteCar);
 router.get("/", CarController.getCars);
 router.post("/", CarController.getCar);
 
